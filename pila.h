67d33f03ecc5f74627dd47938b1395075f029613
Saul-Diaz-Gayosso <62972890+Saul-Diaz-Gayosso@users.noreply.github.com>
2020-04-01 01:38:45 -0600

    Add files via upload

---
+++ pila.h
@@ -0,0 +1,13 @@
+#ifndef __PILA_H__
+#define __PILA_H__
+#include <stdio.h>
+#include <stdlib.h>
+struct Pila{
+   int dato;
+   struct Pila *siguiente;
+};
+void push(struct Pila **MiPila, int num);
+int pop(struct Pila **MiPila);
+int top(struct Pila *MiPila);
+void mostrarpila(struct Pila **MiPila);
+#endif
